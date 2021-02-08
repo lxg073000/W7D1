c448c62438dec8c81e99b35a8b8a4d73ef329dbc
App Academy Student <lxg073000@gmail.com>
2021-02-08 17:49:58 -0500

    WE KILLED IT

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,29 @@ class SessionsController < ApplicationController
     end
 
     def create
-        user = User.find_by_credentials(user_name, password)
+        
+        user = User.find_by_credentials(
+            params[:user][:user_name],
+            params[:user][:password]
+        )
         if user 
-            session[:session_token] = user.reset_session_token
+            session[:session_token] = user.reset_session_token!
+            fail
+            current_user
             redirect_to cats_url
         else
+            @user = User.new
             render :new
         end  
+    end
+
+    def destroy
+        
+        if logged_in?
+            current_user.reset_session_token!
+            session[:session_token] = nil
+            @current_user = nil
+            redirect_to new_session_url
+        end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
 
   attr_reader :password
 
-  def reset_session_token
+  def reset_session_token!
     self.session_token = SecureRandom::urlsafe_base64
     self.save!
     self.session_token
