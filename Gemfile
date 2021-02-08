c1caa2d3e9ed0f3e034ac107f06e0befa3a68ab6
App Academy Student <lxg073000@gmail.com>
2021-02-08 15:02:19 -0500

    Complete 99cats#Usermodels

--- Gemfile
+++ Gemfile
@@ -37,6 +37,12 @@ group :development, :test do
   # Adds support for Capybara system testing and selenium driver
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
+  gem 'annotate'
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'pry-rails'
+  gem 'faker'
+
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+COLOR = ['black', 'white', 'brown', 'tabby']
+SEX = ['M', 'F']
+
+Cat.destroy_all
+ApplicationRecord.connection.reset_pk_sequence!('cats')
+
+20.times do
+  kitty = Cat.create(
+    birth_date: Faker::Date.between(from: '1900-09-23', to: Date.today),
+    color: COLOR.sample,
+    name: Faker::Hipster.word.titleize,
+    sex: SEX.sample,
+    description: Faker::TvShows::Seinfeld.quote
+  )
+
+
+end
